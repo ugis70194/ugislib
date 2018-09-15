22f3bb523204ea22350d5f988945aac3ca74d33d
ugis70194 <40563135+ugis70194@users.noreply.github.com>
2018-09-15 10:27:12 +0900

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 int gcd(int n,int m){return !m?n:gcd(m,n%m);}
 
 //Sieve of Eratosthenes
@@ -20,7 +17,6 @@ using P = pair<int,int>;
 struct edge{int to,cost;};//行き先とそこまでの費用
 vector<edge> G[100100]; //edge型の配列の配列をつくる
                         //各頂点から行ける頂点と費用を記録
-
 vector<int> dijkstra(int start){
     // start = 0;
     vector<int> dist(100100,1e9); dist[start] = 0;
@@ -35,7 +31,7 @@ vector<int> dijkstra(int start){
         //現在地に記録されている費用の和より今までの費用の和が大きいならスキップ
         if(dist[p.second] < p.first) continue;
         for(auto &e : G[p.second]){//現在地から行ける点を列挙
-            if(dist[e.to] > dist[p.second] + e.cost){//現在行き先に記録されている値より、現在地に記録されている値とそこへ行くの費用の和が小さいなら
+            if(dist[e.to] > dist[p.second] + e.cost){//現在行き先に記録されている値より、費用の和が小さいなら
                 dist[e.to] = dist[p.first] + e.cost; //行き先の記録を更新
                 Q.push({dist[e.to],e.to}); //行き先とそこまでの値を突っ込む 
             }
