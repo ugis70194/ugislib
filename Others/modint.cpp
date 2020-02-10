0ec45c79c1456e48a14faf99e9f707e7ca7d89e4
ugis70194 <ugis70194@yahoo.co.jp>
2020-02-10 21:12:54 +0900

    add modint

---
+++ Others/modint.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdint>
+
+struct mint{
+    using i64 = int_fast64_t;
+    i64 x;
+    const i64 MOD = 1000000007;
+
+    mint(const i64 y = (i64)0) noexcept : x(y) { if(x >= MOD) x %= MOD; }
+    mint operator+() const noexcept {return x;}
+    mint operator-() const noexcept {return -x;}
+
+    mint operator+(const mint y) const noexcept {return (mint)*this += y; }
+    mint operator-(const mint y) const noexcept {return (mint)*this -= y; }
+    mint operator*(const mint y) const noexcept {return (mint)*this *= y;}
+    mint operator/(const mint y) const noexcept {return (mint)*this /= y;}
+
+    mint& operator+=(const mint y) noexcept {
+        x += y.x; 
+        if(x >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint y) noexcept {
+        x -= y.x;
+        if(x < 0) x += MOD;
+        return *this;
+    }
+    mint& operator*=(const mint y) noexcept {
+        x *= y.x;
+        if(x >= MOD) x %= MOD;
+        return *this;
+    }
+    mint& operator/=(mint y) noexcept {
+        i64 exp = MOD - 2;
+        while(exp){
+            if(exp & 1) *this *= y;
+            y *= y;
+            exp /= 2;
+        }
+        return *this;
+    }
+    friend std::ostream& operator<< (std::ostream& os, const mint& m);
+    friend std::istream& operator>> (std::istream& in, mint& m);
+};
+std::ostream& operator<<(std::ostream& os, const mint& m){
+    os << m.x;
+    return os;
+}
+std::istream& operator>> (std::istream& in, mint& m){
+    in >> m.x; 
+    if(m.x < 0) m.x += 1000000007;
+    if(m.x >= 1000000007) m.x %= 1000000007;
+    return in; 
+}
